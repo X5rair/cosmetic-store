a8bd04ab8ca4373aca10d4ebe14d8a4be455a66c
Файрушин Ян <sgastana23@gmail.com>
2026-01-15 11:58:19 +0500

    added new columns

--- js/script.js
+++ js/script.js
@@ -1,27 +1,21 @@
-// Получаем элементы из DOM
 var cartBtn = document.getElementById('cartBtn');
 var cartMenu = document.getElementById('cartMenu');
 var cartList = document.getElementById('cartList');
 var addToCartButtons = document.querySelectorAll('.btn-ghost');
-// Корзина как массив
 var cartItems = [];
-// Функция для отображения товаров в корзине
 function renderCart() {
-    cartList.innerHTML = ''; // очищаем список
+    cartList.innerHTML = '';
     cartItems.forEach(function (item) {
         var li = document.createElement('li');
         li.innerHTML = "<img src=\"".concat(item.imgSrc, "\" alt=\"").concat(item.name, "\" width=\"40\"><span>").concat(item.name, "</span>");
         cartList.appendChild(li);
     });
-    // Обновляем счетчик
     var cartCount = document.getElementById('cartCount');
     cartCount.textContent = cartItems.length.toString();
 }
-// Открытие/закрытие корзины
 cartBtn.addEventListener('click', function () {
     cartMenu.classList.toggle('open');
 });
-// Добавление товара в корзину
 addToCartButtons.forEach(function (btn) {
     btn.addEventListener('click', function (e) {
         var _a;
